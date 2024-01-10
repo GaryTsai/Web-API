5d78b72612ecfe7e374bcd7fa77a5d641f011436
gary\user <garybird606@gmail.com>
2024-01-10 11:13:04 +0800

    Add twstock

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 from typing import Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel
+import twstock
 import os
 from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 load_dotenv()
+import json
+import time
+import requests
+
+
+# 參考 twstock 取得需要的 URL
+SESSION_URL = 'http://mis.twse.com.tw/stock/index.jsp'
+STOCKINFO_URL = 'http://mis.twse.com.tw/stock/api/getStockInfo.jsp?ex_ch={stock_id}&_={time}'
+
+
+# def get_realtime_price(stockNo, ex='tse'):
+    
+#     req = requests.Session()
+#     req.get(SESSION_URL)
+    
+#     stock_id = '{}_{}.tw'.format(ex, stockNo)
+#     r = req.get(STOCKINFO_URL.format(stock_id=stock_id, time=int(time.time()) * 1000))
+#     try:
+#         return r.json()['msgArray'][0]['z']
+#     except json.decoder.JSONDecodeError:
+#         return {'rtmessage': 'json decode error', 'rtcode': '5000'}
+# def get_stocks_realtime_price(numberList): 
+#     data = { "stock_realtime_price": { }}
+#     print(numberList.stock_list)
+#     for stock_number in numberList.stock_list:
+#         try:
+#             data["stock_realtime_price"][stock_number] = get_realtime_price(stock_number)
+#         except:
+#             print("Connection refused by the server..")
+#             print("Let me sleep for 5 seconds")
+#             print("ZZzzzz...")
+#             print("Was a nice sleep, now let me continue...")
+
+    # with open('stock_realtime_price.json', 'w') as f:
+    #     json.dump(data, f)
+    # print(data["stock_realtime_price"]["0056"], 333)
+    # return data
+  
 
 key = os.getenv("FAKE_VALUE")
 app = FastAPI()
@@ -30,10 +68,13 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.post("/items/")
-async def create_item(item: Stock):
-    print(item)
-    return item
+@app.post("/realtime_price/")
+async def stock_price(numberList: Stock):
+    data = {}
+    for stock_number in numberList.stock_list:
+        if((stock_number in twstock.codes) == True):
+            data[stock_number] = twstock.realtime.get(stock_number)['realtime']['best_ask_price'][0]
+    return data
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
